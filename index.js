f67a992f50b82097288c7ed3d31d5da7b88cd359
Mai Vang <vmaineng@gmail.com>
2022-04-27 10:31:31 -0700

    commit tree e5404e19efe11c2a1b5d53d5f01f4006f4fe7a88

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"] 
+
+function writeCards(name, event) {
+    let messages = [];
+    for (let i =0; i < name.length; i++){
+        messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number>=0){
+        console.log(number);
+        number--;
+    }
+}
+
